cd52b5f63ad31958f732c35442f2c58f373adbfc
Matt Stein <matt@pixelandtonic.com>
2020-06-17 14:59:23 -0500

    Hide component if there’s nothing to show.

--- docs/.vuepress/theme/components/PostHeading.vue
+++ docs/.vuepress/theme/components/PostHeading.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="post-heading">
-    <div class="auto-toc block xl:hidden">
+    <div class="auto-toc block xl:hidden" v-if="headingItems.length && headingItems[0].children.length">
       <SidebarLinks :depth="0" :items="headingItems" fixed-heading="On this Page" />
     </div>
   </div>
