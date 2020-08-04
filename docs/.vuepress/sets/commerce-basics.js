e59c325dfb197e696ef2b6b1752811289b2d7401
Matt Stein <matt@pixelandtonic.com>
2020-08-03 19:20:26 -0500

    Remove a file.

--- docs/.vuepress/sets/commerce-basics.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  title: "Commerce Basics",
-  icon: "/docs/icons/tutorial.svg",
-  handle: "commerce-basics",
-  baseDir: "tutorials/commerce-basics",
-  searchPlaceholder: "Search the tutorial (Press “/” to focus)",
-  primarySet: false,
-  sidebar: {}
-};
