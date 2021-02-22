423a99fd900a334c5a39b1dacf7fab72e17218ea
Matt Stein <matt@pixelandtonic.com>
2021-02-22 11:22:43 -0600

    Add Story and animate bars.

--- docs/.vuepress/theme/components/LoadingBars.vue
+++ docs/.vuepress/theme/components/LoadingBars.vue
@@ -1,10 +1,61 @@
 <template>
-    <svg width="112" height="148" viewBox="0 0 112 148" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="83" height="13" fill="#BAC0CA"/>
-        <rect y="27" width="112" height="13" fill="#E2E8F0"/>
-        <rect y="54" width="109" height="13" fill="#E2E8F0"/>
-        <rect y="81" width="76" height="13" fill="#E2E8F0"/>
-        <rect y="108" width="96" height="13" fill="#E2E8F0"/>
-        <rect y="135" width="103" height="13" fill="#E2E8F0"/>
-    </svg>
+  <div class="loading-bars">
+    <div class="bar darker animated"></div>
+    <div class="bar animated w-24"></div>
+    <div class="bar animated"></div>
+    <div class="bar animated"></div>
+    <div class="bar animated"></div>
+    <div class="bar animated"></div>
+  </div>
 </template>
+
+<style lang="postcss">
+.loading-bars {
+  .bar {
+    @apply block mb-4;
+    background: #e2e8f0;
+    height: 13px;
+
+    &.darker {
+      background: #bac0ca;
+    }
+
+    &.animated {
+      @apply relative z-0;
+
+      &::before {
+        content: "";
+        @apply absolute z-10 block w-full opacity-75;
+        height: 13px;
+        background: linear-gradient(-90deg, var(--bg-color), transparent);
+        background-width: 26px;
+        animation-name: slide-left-right;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+      }
+    }
+
+    &:nth-child(1) { width: 88px; }
+    &:nth-child(2) { width: 114px; }
+    &:nth-child(2)::before { animation-delay: 0.2s; }
+    &:nth-child(3) { width: 108px; }
+    &:nth-child(3)::before { animation-delay: 0.3s; }
+    &:nth-child(4) { width: 80px; }
+    &:nth-child(4)::before { animation-delay: 0.4s; }
+    &:nth-child(5) { width: 96px; }
+    &:nth-child(5)::before { animation-delay: 0.5s; }
+    &:nth-child(6) { width: 100px; }
+    &:nth-child(6)::before { animation-delay: 0.6s; }
+  }
+}
+
+@keyframes slide-left-right {
+  from {
+    transform: translateX(-100%);
+  }
+
+  to {
+    transform: translateX(100%);
+  }
+}
+</style>
