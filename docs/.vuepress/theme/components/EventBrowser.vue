305591e1f0ad6f2a910079db0e3ce8244f458bec
Matt Stein <matt@pixelandtonic.com>
2021-12-07 12:00:13 -0500

    Tidy up a bit.

--- docs/.vuepress/theme/components/EventBrowser.vue
+++ docs/.vuepress/theme/components/EventBrowser.vue
@@ -55,7 +55,6 @@
             }"
           >
             <div class="w-4/5 flex items-center content-center">
-              <!-- <span class="block">{{ filterType }}</span> -->
               <span v-if="filterData.label" class="block">
                 {{ filterData.label }}
               </span>
@@ -154,7 +153,6 @@
                     <path
                       fill="currentColor"
                       d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
-                      class=""
                     ></path>
                   </svg>
                 </label>
@@ -366,7 +364,6 @@ export default {
       codeCopied: false
     };
   },
-  mounted() {},
   methods: {
     getClassName(fullClass) {
       return fullClass.split("\\").pop();
