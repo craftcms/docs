3f0ddf00ccfe76b4f313e0719056f4d50a770315
Matt Stein <matt@pixelandtonic.com>
2022-02-02 13:14:42 -0800

    Remove stale translations.

--- docs/.vuepress/sets/craft-cms-fr.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  selectText: "Language",
-  label: "French",
-  disabled: true,
-  editLinkText: "Edit this page on GitHub",
-  searchPlaceholder: "Search the Craft docs (Press “/” to focus)",
-  sidebar: {}
-};
