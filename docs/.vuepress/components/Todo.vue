1c47d9fbfc446cf995b1d4fa5e1b3e48787a17a2
August Miller <hello@gusmiller.com>
2022-10-25 18:28:55 -0700

    Todo component for warning about edits
    Could do this with plain text search in an IDE, but I kind of like the notion of being able to build a Vue store of all the Todos throughout the system, or even integrate it with the build process (i.e. more than zero "severe" todos would fail tests).
    
    This could also just incentivize deleting todos, or converting them to comments :weary:

---
+++ docs/.vuepress/components/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="todo" :text="notes"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    ver: String,
+    notes: {
+      type: String,
+      default: 'No notes provided.',
+    },
+  },
+  computed: {},
+  mounted () {
+    console.warn(`[${this.$page.title}] To Do: ${this.notes}`);
+  }
+};
+</script>
+
+<style lang="postcss" scoped>
+.todo {
+    display: none;
+}
+</style>
