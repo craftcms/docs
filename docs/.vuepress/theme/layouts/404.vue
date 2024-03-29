0122d18fe4d397d774381c9e64614601a6352660
August Miller <hello@gusmiller.com>
2024-03-29 10:43:39 -0700

    404 + Broken link issue template

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -5,26 +5,27 @@
 
       <blockquote>{{ getMsg() }}</blockquote>
 
-      <RouterLink to="/">
-        Take me home.
-      </RouterLink>
+      <RouterLink to="/">Take me home</RouterLink> or <a :href="getIssueUrl()">report a broken link</a>.
     </div>
   </div>
 </template>
 
 <script>
 const msgs = [
-  `There's nothing here.`,
+  `There’s nothing here.`,
   `How did we get here?`,
   `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
+  `Looks like we've got some broken links.`,
+];
 
 export default {
   methods: {
     getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
-  }
-}
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    },
+    getIssueUrl () {
+      return `https://github.com/craftcms/docs/issues/new/choose?template=BROKEN_LINK.yml&url=${window.location.href}`
+    },
+  },
+};
 </script>
