81602efe11aa25602f118137a177a637311d889f
August Miller <hello@gusmiller.com>
2024-04-12 14:57:28 -0700

    Output JSON “sitemap”

--- docs/.vuepress/plugins/craft.js
+++ docs/.vuepress/plugins/craft.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs');
 const { replacePrefixes } = require("../theme/util/replace-anchor-prefixes");
 
 /**
@@ -11,5 +13,29 @@ module.exports = function(options, ctx) {
                 return replacePrefixes(md, ctx);
             });
         },
+        async generated () {
+            const manifest = [];
+
+            ctx.pages.forEach(function(p) {
+                manifest.push({
+                    title: p.title,
+                    path: p.path,
+                    summary: p.frontmatter.description || p.excerpt || null,
+                    keywords: (p.frontmatter.keywords || '').split(' '),
+                });
+            });
+
+            await fs.writeFile(
+                path.resolve(ctx.outDir, 'sitemap.js'),
+                JSON.stringify(manifest, null, 4),
+                function(err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    console.log("Wrote sitemap.js!");
+                },
+            );
+        }
     };
 };
