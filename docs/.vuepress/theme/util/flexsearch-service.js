3fbd34467129a05e038d59d68b880887cb4211d1
August Miller <hello@gusmiller.com>
2025-10-22 13:11:06 -0700

    lol

--- docs/.vuepress/theme/util/flexsearch-service.js
+++ docs/.vuepress/theme/util/flexsearch-service.js
@@ -1,4 +1,4 @@
-import Flexsearch from "flexsearch";
+import FlexSearch from "flexsearch";
 import _ from "lodash";
 
 const defaultLang = "en-US";
@@ -32,7 +32,7 @@ export default {
       }
     };
 
-    const globalIndex = new Flexsearch(indexSettings);
+    const globalIndex = new FlexSearch(indexSettings);
     globalIndex.add(
       pages.filter(page => {
         // default language, primary set, and primary version (designated or versionless set)
