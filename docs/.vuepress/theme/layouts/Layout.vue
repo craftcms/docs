539238f7206c2113a36fbc17a1a23fc2f93b160b
August Miller <hello@gusmiller.com>
2025-11-12 14:27:35 -0800

    Layout stability, remove touch/swipe handlers

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,10 +1,5 @@
 <template>
-  <div
-    class="theme-container"
-    :class="pageClasses"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
-  >
+  <div class="theme-container" :class="pageClasses">
     <span ref="backToTop" tabindex="-1" />
     <ul class="skip-links">
       <li>
@@ -66,7 +61,7 @@
 }
 
 .theme-container {
-  @apply relative w-full;
+  @apply relative w-full overflow-x-hidden;
 }
 
 .sidebar-transitioning {
@@ -485,26 +480,6 @@ export default {
       }
 
       return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-    },
-
-    // side swipe
-    onTouchStart(e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY,
-      };
-    },
-
-    onTouchEnd(e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x;
-      const dy = e.changedTouches[0].clientY - this.touchStart.y;
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true);
-        } else {
-          this.toggleSidebar(false);
-        }
-      }
     },
 
     temporarilyAnimateBody() {
