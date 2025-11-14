c60965483b3fc4863a4ef40279d1e5f1186f4ce2
August Miller <hello@gusmiller.com>
2025-11-13 16:43:32 -0800

    Figure component? I'd like to be able to caption images, but… this didn't work.

---
+++ docs/.vuepress/components/Fig.vue
@@ -0,0 +1,15 @@
+<template>
+    <figure>
+        <img :src="src">
+        <figcaption :html="caption"></figcaption>
+    </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+    caption: String,
+  },
+};
+</script>
